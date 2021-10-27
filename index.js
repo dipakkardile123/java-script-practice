6ddb1205936f18d3a87ee20a11346752cbf18598
dipakkardile123 <dipakkardile71@gmail.com>
2021-10-27 15:38:21 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-let eligible = true
-required = true
-console.log(eligible && required);
-let b = true;
-let result = b && (0 / 1)
-console.log(result)
+let eligible = false,
+    required = true
+console.log(!eligible);
+console.log(!required)
+
+console.log(!undefined);
+console.log(!null);
+console.log(!20);
+console.log(!0);
+console.log(!NaN);
+console.log(!{});
+console.log(!'');
+console.log(!'ok');
+console.log(!false);
+console.log(!true);
+
+let counter = 10
+console.log("counter", !!counter);
